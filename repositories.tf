4b7cbc43dc559c3af1190348d65a9796cf512aec
Kenny Carter <kennycocopuff@gmail.com>
2025-02-26 19:48:19 -0800

    update to github teams

--- repositories.tf
+++ repositories.tf
@@ -6,3 +6,22 @@ resource "github_repository" "this" {
   allow_squash_merge = true
 
 }
+
+resource "github_team" "dingusoars" {
+  name        = "dingusoars"
+  description = "go team dingusoar"
+}
+
+resource "github_repository_collaborators" "this" {
+  repository = github_repository.this.name
+
+  user {
+    permission = "admin"
+    username  = "senpaikcarter"
+  }
+
+  team {
+    permission = "maintain"
+    team_id = github_team.dingusoars.id
+  }
+}
